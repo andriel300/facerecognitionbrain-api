c80a7a1fc5b80839cceacffcc431f6d3c50f208f
andriel300 <andrielscript@gmail.com>
2023-02-05 15:17:34 -0300

    database url

--- controllers/db.js
+++ controllers/db.js
@@ -6,13 +6,12 @@ dotenv.config();
 
 const db = knex({
   client: "pg",
-  connection: {
-    host: process.env.DB_HOST,
-    port: 5432,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  },
+  connection: process.env.DATABASE_URL,
+  // host: process.env.DB_HOST,
+  // port: 5432,
+  // user: process.env.DB_USERNAME,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_NAME,
 });
 
 module.exports = { db, bcrypt };
